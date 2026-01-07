302bae0a543334b87e8a1691a76a865a1205b458
ThisanduSSR <thisanduranadheera67@gmail.com>
2026-01-07 19:03:50 +0530

    Component Export Statement
    This statement makes the Features Page available for use in other files.

--- pages/FeaturesPage.tsx
+++ pages/FeaturesPage.tsx
@@ -26,6 +26,9 @@ const FeaturesPage = () => {
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                 {features.map(feature => <FeatureCard key={feature.title} {...feature} />)}
+            </div>
         </div>      
-    )
-}
+    );
+};
+
+export default FeaturesPage;
