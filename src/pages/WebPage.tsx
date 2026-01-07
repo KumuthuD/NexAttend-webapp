0f006795ad6ea11843de95cb6b86c18a1a64487d
Viraj Jayasiri <viraj.jayasiri.work@gmail.com>
2026-01-07 21:26:40 +0530

    Add background video section container

--- src/pages/WebPage.tsx
+++ src/pages/WebPage.tsx
@@ -3,10 +3,22 @@ import { Link } from 'react-router-dom';
 import demoVideo from '../components/signup-page.mp4';
 
 const WebPage = () => {
-  return (
-    <div className="relative h-screen flex flex-col overflow-hidden">
-    </div>
-  );
+    return (
+        <div className="relative h-screen flex flex-col overflow-hidden">
+            {/* Background Video Section */}
+            <div className="relative w-full h-full overflow-hidden">
+                {/* Video Background */}
+                <video
+                    className="absolute top-0 left-0 w-full h-full object-cover"
+                    src={demoVideo}
+                    autoPlay
+                    muted
+                    loop
+                    playsInline
+                />
+                                </div>
+            </div>
+    );
 };
 
 export default WebPage;
