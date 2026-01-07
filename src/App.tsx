a744ab23f5fe4d909916891fdaee53c92ffb3316
Yasitha Peiris <yasithapeiris68@gmail.com>
2026-01-07 21:08:04 +0530

    remove previous header an footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
 import LandingPage from './pages/LandingPage';
 import FeaturesPage from './pages/FeaturesPage';
 import AboutPage from './pages/AboutPage';
@@ -10,11 +8,9 @@ import WebPage from './pages/WebPage';
 
 const Layout = () => {
     const location = useLocation();
-    const showFooter = location.pathname !== '/webpage';
 
     return (
         <div className="bg-gray-900 text-white min-h-screen font-sans">
-            <Header />
             <Routes>
                 <Route path="/" element={<LandingPage />} />
                 <Route path="/features" element={<FeaturesPage />} />
@@ -22,7 +18,6 @@ const Layout = () => {
                 <Route path="/contact" element={<ContactPage />} />
                 <Route path="/webpage" element={<WebPage />} />
             </Routes>
-            {showFooter && <Footer />}
         </div>
     );
 };
