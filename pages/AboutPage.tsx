b94b74c25ca525bd4efec60aefe44bd425ecd618
sudamamarajeewa <sudamamarajeewa329@gmail.com>
2026-01-07 09:47:31 +0530

    Imports
    add imports for About page

---
+++ pages/AboutPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { LinkedInIcon, InstagramIcon, XIcon } from '../components/icons';
+
+import kumuthuImg from '../components/kumuthu.jpg';
+import thisanduImg from '../components/thisandu.jpg';
+import thiviruImg from '../components/thiviru.jpg';
+import yasithaImg from '../components/yasitha.jpg';
+import virajImg from '../components/viraj.jpg';
+import sudamImg from '../components/sudam.jpg';
