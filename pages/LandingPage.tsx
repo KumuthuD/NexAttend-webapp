f35cae3de7e6145e9f0c6c9c8f81e828c64f21f7
KumuthuD <kumuthudahanayake@gmail.com>
2026-01-07 08:56:09 +0530

    landing page starting
    Initial Imports and Component Setup

--- pages/LandingPage.tsx
+++ pages/LandingPage.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import FeatureCard from '../components/FeatureCard';
-import demoVideo from '../components/nexattend-demo.mp4';
-import { CheckCircleIcon, BrainIcon, ShieldCheckIcon, UsersIcon } from '../components/icons';
 
+const LandingPage: React.FC = () => {
+  return (
+    <div className="landing-page">
+      {/* Content will be added in subsequent commits */}
+    </div>
+  );
+};
+
+export default LandingPage;
