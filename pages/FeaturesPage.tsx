ec788927ff03f5727f617694d14d9b4dec001d23
ThisanduSSR <thisanduranadheera67@gmail.com>
2026-01-07 10:06:42 +0530

    component import
    This part imports all required components, images, and styles needed for the Features Page.

---
+++ pages/FeaturesPage.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import FeatureCard from '../components/FeatureCard';
+import {
+    CameraIcon, UsersIcon, CodeBracketIcon, CheckCircleIcon, ClockIcon, EnvelopeIcon, SparklesIcon, ChatBubbleLeftRightIcon, CharBarIcon, DocumnetTextIcon
+} from '../components/icons';
