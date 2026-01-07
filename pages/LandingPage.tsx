be86307090b2bc9b7c280a57d27567d0513d5066
KumuthuD <kumuthudahanayake@gmail.com>
2026-01-07 09:02:25 +0530

    hero section
    Add Background Video Hero Section

--- pages/LandingPage.tsx
+++ pages/LandingPage.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import backgroundVideo from '../assets/background.mp4';
 
 const LandingPage: React.FC = () => {
   return (
     <div className="landing-page">
-      {/* Content will be added in subsequent commits */}
+      <section className="relative w-full h-screen overflow-hidden">
+        <video
+          className="absolute top-0 left-0 w-full h-full object-cover"
+          src={backgroundVideo}
+          autoPlay
+          loop
+          muted
+        />
+      </section>
     </div>
   );
 };
