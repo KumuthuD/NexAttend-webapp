05871f2b7eb3266e4a9531f87eeab2305840cdc7
Thiviru Igalawithana <thivirudamsith7715@gmail.com>
2026-01-07 14:42:20 +0530

    implement contact page with basic layout and social media icons

---
+++ src/pages/ContactPage.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ContactImg from '../components/Contact.png';
+import { InstagramIcon, LinkedInIcon, XIcon } from '../components/icons';
+
+const ContactPage = () => (
+    <div className="py-16 px-4 container mx-auto">
+        <div className="text-center mb-12">
+            <h2 className="text-5xl font-extrabold text-pink-500 mb-4">Get In Touch</h2>
+            <p className="text-xl text-gray-400">We'd love to hear from you. Reach out with questions or for a demo.</p>
+        </div>
+
+        
+
+        
+    </div>
+);
+
+export default ContactPage;
