5e8a34f5567430376dc7027213bfee14641e838f
KumuthuD <kumuthudahanayake@gmail.com>
2026-01-07 08:46:40 +0530

    Update the landing page
    update imports for key components

---
+++ pages/LandingPage.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import FeatureCard from '../components/FeatureCard';
+import demoVideo from '../components/nexattend-demo.mp4';
+import { CheckCircleIcon, BrainIcon, ShieldCheckIcon, UsersIcon } from '../components/icons';
+
