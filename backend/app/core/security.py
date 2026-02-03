93fea9f3c9c02438cae332f622d942ba34d41cf2
sudamamarajeewa <sudamamarajeewa329@gmail.com>
2026-02-03 13:35:01 +0530

    feat(backend): implement create_access_token in security.py

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -20,3 +20,20 @@ def get_password_hash(password: str) -> str:
     Generate a bcrypt hash of a plain password.
     """
     return pwd_context.hash(password)
+
+
+def create_access_token(
+    subject: Union[str, Any], expires_delta: timedelta = None
+) -> str:
+    """
+    Generate a JWT access token.
+    """
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(
+            minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
+        )
+    to_encode = {"exp": expire, "sub": str(subject)}
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
